701cb89fd28bc520ea647f4a48731ad690f3bb66
Unknown <das7pad@outlook.com>
2017-11-03 22:59:46 +0100

    [update-messages] drop not needed `rm` call and reduce verbose logging

--- update-messages.sh
+++ update-messages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
+echo "Updating messages"
+
 DIR="`dirname $0`/"
 xgettext --files-from="$DIR"POTFILES.in --directory="$DIR".. --output="$DIR"messages.pot
-echo "messages.pot generated in $DIR"
-rm -rf "$DIR"en/LC_MESSAGES/hangupsbot.po
 msgen messages.pot --directory="$DIR" -o "$DIR"en/LC_MESSAGES/hangupsbot.po
-echo "and completed the en file"
