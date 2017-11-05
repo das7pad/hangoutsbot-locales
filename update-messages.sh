54949a662f4928451bdf2fa0e106981374eca420
Unknown <das7pad@outlook.com>
2017-11-05 09:31:09 +0100

    [update-messages] rename the variable `DIR` to `LOCALE_DIR`

--- update-messages.sh
+++ update-messages.sh
@@ -3,19 +3,19 @@
 echo "Updating messages"
 
 # change base dir to parent repo level
-DIR="$(cd "$(dirname $0)" && pwd)"
-cd "$DIR/../../"
+LOCALE_DIR="$(cd "$(dirname $0)" && pwd)"
+cd "$LOCALE_DIR/../../"
 
 # update python-file list
-find hangupsbot -name '*.py' > $DIR/POTFILES.in
+find hangupsbot -name '*.py' > $LOCALE_DIR/POTFILES.in
 
-TARGET=$DIR/messages.pot
+TARGET=$LOCALE_DIR/messages.pot
 
 # find strings per module
-xgettext --files-from $DIR/POTFILES.in --output $TARGET
+xgettext --files-from $LOCALE_DIR/POTFILES.in --output $TARGET
 
 # update charset to `UTF-8`
 sed -r "s/charset=CHARSET/charset=UTF-8/" --in-place $TARGET
 
 # clear the english template
-msgen $TARGET --output-file $DIR/en/LC_MESSAGES/hangupsbot.po
+msgen $TARGET --output-file $LOCALE_DIR/en/LC_MESSAGES/hangupsbot.po
