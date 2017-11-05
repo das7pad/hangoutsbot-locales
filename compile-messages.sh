1f56523150eb76da6695871474ca7cdc0e34a84c
Unknown <das7pad@outlook.com>
2017-11-05 09:34:29 +0100

    [compile-messages] add an option for verbose logging

--- compile-messages.sh
+++ compile-messages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run with additional argument to see a process log
 
 echo "Compiling messages"
 
@@ -6,7 +7,16 @@ echo "Compiling messages"
 LOCALE_DIR="$(cd "$(dirname $0)" && pwd)"
 cd $LOCALE_DIR
 
+LOG_VERBOSE="$1"
+
+log_complete() {
+    if [ ! -z $LOG_VERBOSE ]; then \
+        echo " compiled: ${1^^}"
+    fi
+}
+
 for LOCALE in `find * -maxdepth 0 -type d`; do \
     LOCALE_DIR=$LOCALE/LC_MESSAGES
     msgfmt "$LOCALE_DIR"/hangupsbot.po --output-file $LOCALE_DIR/hangupsbot.mo
+    log_complete $LOCALE
 done
