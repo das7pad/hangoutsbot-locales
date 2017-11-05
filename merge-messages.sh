766ed07977ab444829906147a3a3252fc27d290f
Unknown <das7pad@outlook.com>
2017-11-05 09:32:32 +0100

    [merge-messages] add an option for verbose logging

--- merge-messages.sh
+++ merge-messages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run with additional argument to see a process log
 
 echo "Merging messages"
 
@@ -6,10 +7,19 @@ echo "Merging messages"
 LOCALE_DIR="$(cd "$(dirname $0)" && pwd)"
 cd $LOCALE_DIR
 
+LOG_VERBOSE="$1"
+
+log_complete() {
+    if [ ! -z $LOG_VERBOSE ]; then \
+        echo "  merged: ${1^^}"
+    fi
+}
+
 # update paths/lines of already translated entrys and add new entrys
 # as the english version is already processed, we drop it here in the head
 for LOCALE in `find * -maxdepth 0 -type d -not -path "en"`; do \
     LOCALE_DIR=$LOCALE/LC_MESSAGES
     msgmerge -q --update --no-fuzzy-matching --backup=off \
         $LOCALE_DIR/hangupsbot.po messages.pot
+    log_complete $LOCALE
 done
