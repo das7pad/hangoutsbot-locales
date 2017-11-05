21cff031e6977d7e50a05f931a10069d6475f6c5
Unknown <das7pad@outlook.com>
2017-11-05 09:11:31 +0100

    [update-messages] use an absolute path for the locale dir

--- update-messages.sh
+++ update-messages.sh
@@ -2,6 +2,6 @@
 
 echo "Updating messages"
 
-DIR="`dirname $0`/"
+DIR="$(cd "$(dirname $0)" && pwd)/"
 xgettext --files-from="$DIR"POTFILES.in --directory="$DIR".. --output="$DIR"messages.pot
 msgen messages.pot --directory="$DIR" -o "$DIR"en/LC_MESSAGES/hangupsbot.po
